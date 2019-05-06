806431e9d29fe7099ab27f5053c8a546d0e52a43
undwin <love@und.win>
2019-05-06 17:58:21 +0800

    提交

---
+++ demo-activemq/src/main/java/und/demo/ActiveMQSpringBootApplication.java
@@ -0,0 +1,11 @@
+package und.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMQSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMQSpringBootApplication.class,args);
+    }
+}
